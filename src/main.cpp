7a49efe8c4be121a65696beb66a257323fd6b7c5
KillerSpartan <al1709130@upy.edu.mx>
2020-05-18 12:33:40 -0500

    Dos barcos Posicionados con Coordenadas

--- src/main.cpp
+++ src/main.cpp
@@ -101,18 +101,27 @@ int Value = 0;
 int positionY = 0;
 int positionX = 2;
 
-//Barcos
+//Barcos for Player 1
 int oneX = 18, oneY = 2;
 int twoX = 18, twoY=2;
 int threeX = 18, threeY = 2;
+
+//Barcos for Player 2
+int p2_oneX = 18, p2_oneY = 2;
+int p2_twoX = 18, p2_twoY=2;
+int p2_threeX = 18, p2_threeY = 2;
 
 //Player one
 int P1BarcoOne[]= {0, 0};
 int P1BarcoTwo[]= {0, 0};
 int P1BarcoTre[]= {0, 0};
 
+//Player 2
+int P2BarcoOne[]= {0, 0};
+int P2BarcoTwo[]= {0, 0};
+int P2BarcoTre[]= {0, 0};
 
-int numBP1 = 0;
+int numBP1 = 0, numBP2 = 0; //Cantidad de Barcos
 char Position[14];
 char incomingByte;
 
@@ -184,8 +193,7 @@ int frameForGame(int x, int y, int size){
     }
   }
   delay(50);
-  return 0;
-} //Esta es la pantalla para P1
+  return 0;} //Esta es la pantalla para P1
 int P2ForGame(int x, int y, int size){
 
   lcd.setCursor(0,0);lcd.write(4);
@@ -203,30 +211,30 @@ int P2ForGame(int x, int y, int size){
 
    case 1 :
    lcd.setCursor(x,y);
-   if(numBP1==3){x=threeX; y=threeY;}
+   if(numBP2==3){x=p2_threeX; y=p2_threeY;}
    lcd.write(3);
    break;
  }
 
- if(numBP1==1){x=oneX; y=oneY;}
- if(numBP1==2){x=twoX; y=twoY;}
+ if(numBP2==1){x=p2_oneX; y=p2_oneY;}
+ if(numBP2==2){x=p2_twoX; y=p2_twoY;}
 
- lcd.setCursor(oneX,oneY);
+ lcd.setCursor(p2_oneX,p2_oneY);
  lcd.write(2);
- lcd.setCursor(oneX-1,oneY);
+ lcd.setCursor(p2_oneX-1,p2_oneY);
  lcd.write(1);
 
- lcd.setCursor(twoX,twoY);
+ lcd.setCursor(p2_twoX,p2_twoY);
  lcd.write(2);
- lcd.setCursor(twoX-1,twoY);
+ lcd.setCursor(p2_twoX-1,p2_twoY);
  lcd.write(1);
 
- lcd.setCursor(threeX, threeY);
+ lcd.setCursor(p2_threeX, p2_threeY);
  lcd.write(3);
 
 //(i!=oneX) && (i != oneX-1) && (i!=twoX) && (i!=twoX-1) && (i!=threeX) && (i!=oneY) && (i!=twoY)
   for (int i = 1; i < 16; i++) {
-    if((i!=oneX && i!=oneX-1)&&(i!=twoX && i!=twoX-1)&&(i!=threeX)){
+    if((i!=p2_oneX && i!=p2_oneX-1)&&(i!=p2_twoX && i!=p2_twoX-1)&&(i!=p2_threeX)){
       lcd.setCursor(i, 0);
       lcd.write(8);
       lcd.setCursor(i, 1);
@@ -372,7 +380,6 @@ void loop(){
         P1BarcoTre[0]=threeX; P1BarcoTre[1] = threeY;
 
         lcd.clear();
-        delay(3800);
 
         Serial.print(P1BarcoOne[0]);
         Serial.print(",");
@@ -396,6 +403,9 @@ void loop(){
         break;
 //·················Cambio de Turno···························
       case 8:
+      delay(2000);
+      positionX=2;
+      positionY=0;
       lcd.setCursor(0, 0);
       lcd.print("Es turno de P2");
       lcd.setCursor(0, 1);
@@ -405,11 +415,132 @@ void loop(){
       break;
 
       case 9:
+
+        P2ForGame(p2_oneX,p2_oneY, 0);
+        if (Serial.available() > 0) {
+          incomingByte = Serial.read();
+          posicionamiento(incomingByte);
+                // say what you got:
+          Serial.print("I received: ");
+          Serial.println(incomingByte, DEC);
+          delay(10);
+        }
       break;
+
+      case 10:
+      p2_oneX = positionX;
+      p2_oneY = positionY;
+      P2ForGame(p2_oneX,p2_oneY, 0);
+      numBP2++;
+      sel = 11;
+      break;
+
+      case 11:
+      P2ForGame(p2_twoX,p2_twoY, 0);
+      if (Serial.available() > 0) {
+        incomingByte = Serial.read();
+        posicionamiento(incomingByte);
+        // say what you got:
+       Serial.print("I received: ");
+       Serial.println(incomingByte, DEC);
+       delay(10);}
+      break;
+
+      case 12:
+      p2_twoX = positionX;
+      p2_twoY = positionY;
+      P2ForGame(p2_twoX,p2_twoY, 0);
+      numBP2++;
+      sel = 13;
+      break;
+
+      case 13:
+      P2ForGame(p2_threeX,p2_threeY, 1);
+      if (Serial.available() > 0) {
+        incomingByte = Serial.read();
+        posicionlast(incomingByte);
+        // say what you got:
+       Serial.print("I received: ");
+       Serial.println(incomingByte, DEC);
+       delay(10);
+      }
+      break;
+
+      case 14:
+      p2_threeX = positionX;
+      p2_threeY = positionY;
+      P2ForGame(p2_threeX,p2_threeY, 1);
+      numBP2++;
+      sel=15;
+      break;
+
+      case 15:
+      P2BarcoOne[0]=p2_oneX; P2BarcoOne[1] = p2_oneY;
+      P2BarcoTwo[0]=p2_twoX; P2BarcoTwo[1] = p2_twoY;
+      P2BarcoTre[0]=p2_threeX; P2BarcoTre[1] = p2_threeY;
+
+      //lcd.clear();
+      //delay(3800);
+      Serial.println("Ubicacion Barco Uno");
+      Serial.print(P1BarcoOne[0]);
+      Serial.print(",");
+      Serial.print(P1BarcoOne[1]);
+      Serial.println("  Barco Uno");
+      delay(400);
+
+      Serial.print(P1BarcoTwo[0]);
+      Serial.print(",");
+      Serial.print(P1BarcoTwo[1]);
+      Serial.println("  Barco Dos");
+      delay(400);
+
+      Serial.print(P1BarcoTre[0]);
+      Serial.print(",");
+      Serial.print(P1BarcoTre[1]);
+      Serial.println("  Barco Tres");
+      delay(400);
+
+      Serial.println("Ubicacion Barco Dos");
+      Serial.print(P2BarcoOne[0]);
+      Serial.print(",");
+      Serial.print(P2BarcoOne[1]);
+      Serial.println("  Barco Uno (P2)");
+      delay(400);
+
+      Serial.print(P2BarcoTwo[0]);
+      Serial.print(",");
+      Serial.print(P2BarcoTwo[1]);
+      Serial.println("  Barco Dos(P2)");
+      delay(400);
+
+      Serial.print(P2BarcoTre[0]);
+      Serial.print(",");
+      Serial.print(P2BarcoTre[1]);
+      Serial.println("  Barco Tres(P2)");
+      delay(400);
+
+      //sel = 16;
+      break;
+
+      case 16:
+      lcd.clear();
+      delay(500);
+      sel=17;
+      break;
+
+      case 17:
+      lcd.setCursor(0,0);
+      lcd.print("El juego comienza");
+      break;
+
+
 }
 
 
-if(numBP1>1){lcd.setCursor(positionX, positionY); lcd.write(3); delay(50);}
+
+//·········Movimiento de barcos en la LCD········33
+
+if((numBP1>1)||(numBP2>1)){lcd.setCursor(positionX, positionY); lcd.write(3); delay(50);}
 else{
   lcd.setCursor(positionX, positionY);
   lcd.write(2);
